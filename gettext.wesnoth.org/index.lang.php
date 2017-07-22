3d9d08f1c64b6da43b67c61a537b72b761f9766d
Ignacio R. Morelle <shadowm@wesnoth.org>
2017-07-22 02:19:30 -0400

    gettext.w.o: Backend cleanup and frontend refresh for Wesmere (language view)

--- gettext.wesnoth.org/index.lang.php
+++ gettext.wesnoth.org/index.lang.php
@@ -1,11 +1,12 @@
 <?php
 
+define('IN_WESNOTH_LANGSTATS', true);
+
 include('config.php');
 include('functions.php');
+include('functions-web.php');
 include('langs.php');
-
-global $langs;
-global $branch;
+include('wesmere.php');
 
 $existing_packs         = explode(' ', $packages);
 $existing_corepacks     = explode(' ', $corepackages);
@@ -17,18 +18,26 @@ sort($existing_extra_packs_b);
 
 $stats = [];
 
-// FIXME: not the same default as index.php for some reason?
-$version = isset($_GET['version']) ? parameter_get('version') : 'master';
+//
+// Process URL parameters
+//
+
+// Set the default starting point when calling gettext.wesnoth.org:
+//   'branch': show stats from the current stable branch
+//   'master': show stats from master
+$version = isset($_GET['version']) ? parameter_get('version') : 'branch';
 
 $lang = isset($_GET['lang']) ? parameter_get('lang') : '';
 
-if ($lang != '')
-{
-	$j = 0;
+$nostats = false;
 
+if (!empty($lang))
+{
 	for ($i = 0; $i < 2; ++$i)
 	{
-		if ($i == 0)
+		$official = $i == 0;
+
+		if ($official)
 		{
 			$packs = $existing_packs;
 		}
@@ -39,163 +48,115 @@ if ($lang != '')
 
 		foreach ($packs as $pack)
 		{
-			if ($i == 1)
+			if (!$official)
 			{
 				$pack = getdomain($pack);
 			}
 
-			$statsfile = $version . 'stats';
+			$statsfile = 'stats/' . $pack . '/' . $version . 'stats';
 
-			if (!file_exists('stats/' . $pack . '/' . $statsfile))
+			if (!file_exists($statsfile))
 			{
 				continue;
 			}
 
-			$serialized = file_get_contents('stats/' . $pack . '/' . $statsfile);
+			$serialized = file_get_contents($statsfile);
 			$tmpstats = unserialize($serialized);
 
 			$stat = $tmpstats[$lang];
-			$stats[$j] = [];
-			$stats[$j][0] = $stat[0];	//errors
-			$stats[$j][1] = $stat[1];	//translated
-			$stats[$j][2] = $stat[2];	//fuzzy
-			$stats[$j][3] = $stat[3];	//untranslated
-			$stats[$j][4] = $pack;		//package name
-			$stats[$j][5] = $tmpstats['_pot'][1] + $tmpstats['_pot'][2] + $tmpstats['_pot'][3];
-			$stats[$j][6] = $i;			//official
-
-			++$j;
+			$stats[] = [
+				$stat[0],	// errors
+				$stat[1],	// translated
+				$stat[2],	// fuzzy
+				$stat[3],	// untranslated
+				$pack,		// textdomain name
+				$tmpstats['_pot'][1] + $tmpstats['_pot'][2] + $tmpstats['_pot'][3],
+				$official,	// is official
+			];
 		}
 	}
-
-	$nostats = 0;
 }
 else
 {
-	$nostats = 1;
+	$nostats = true;
 	unset($lang);
 }
 
-$firstpack = $existing_packs[0];
-$statsfile = $version . 'stats';
-$filestat = stat('stats/' . $firstpack . '/' . $statsfile);
-$date = $filestat[9];
+wesmere_emit_header();
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-	<meta http-equiv="content-type" content="text/xhtml; charset=utf-8" />
-	<link rel="shortcut icon" type="image/png" href="/mw/skins/glamdrol/ico.png" />
-	<link rel="stylesheet" href="/mw/skins/glamdrol/main.css" />
-	<link rel="stylesheet" type="text/css" href="styles/old.css" />
-	<title>Translation statistics - Battle for Wesnoth</title>
-</head>
+?>
 
-<body>
+<h1>Translation Statistics</h1>
 
-<div id="global">
-
-<div id="header">
-	<div id="logo">
-		<a href="/"><img alt="Wesnoth logo" src="/mw/skins/glamdrol/wesnoth-logo.jpg" /></a>
-	</div>
-</div>
-
-<div id="nav">
-	<ul>
-		<li><a href="/">Home</a></li>
-		<li><a href="//wiki.wesnoth.org/Play">Play</a></li>
-		<li><a href="//wiki.wesnoth.org/Create">Create</a></li>
-		<li><a href="//forums.wesnoth.org/">Forums</a></li>
-		<li><a href="//wiki.wesnoth.org/Support">Support</a></li>
-		<li><a href="//wiki.wesnoth.org/Project">Project</a></li>
-		<li><a href="//wiki.wesnoth.org/Credits">Credits</a></li>
-		<li><a href="//wiki.wesnoth.org/UsefulLinks">Links</a></li>
-	</ul>
-</div>
-
-<h2 style="display:inline">Wesnoth translation stats</h2>
-(last update: <strong><?php echo date('r', $date) ?></strong>)
-
-<table class="main" cellpadding="1" cellspacing="0" border="0" width="100%"><tr><td>
-
-<table class="title" cellpadding="0" cellspacing="0" border="0" width="100%"><tr><td>
-
-<table cellpadding="2" cellspacing="0" border="0" width="100%"><!-- language and package selection -->
-	<tr>
-		<td align="left">Version:<?php
-			if ($version == 'master')
-			{
-				?><strong>Development</strong> || <a href="?version=branch&amp;package=<?php echo $package ?>&amp;lang=<?php echo $lang ?>"><?php echo $branch ?></a><?php
-			}
-			else
-			{
-				?><a href="?version=master&amp;package=<?php echo $package ?>&amp;lang=<?php echo $lang ?>">Development</a>  || <strong><?php echo $branch ?></strong><?php
-			}
-		?></td>
-	</tr>
-	<tr>
-		<td align="left">Show:
-			<a href="index.php?package=alloff&amp;order=trans&amp;version=<?php echo $version ?>">Official packages</a>
-			|| <a href="index.php?package=allcore&amp;order=trans&amp;version=<?php echo $version ?>">Official core packages</a>
-			|| <a href="index.php?package=all&amp;order=trans&amp;version=<?php echo $version ?>">All packages</a>
-			|| <a href="index.php?package=allun&amp;order=trans&amp;version=<?php echo $version ?>">All unofficial packages</a>
-			|| <strong>By language</strong>
-			<br/>Language:<?php
-			$first = true;
-			foreach ($langs as $code => $langname)
-			{
-				if ($first)
-				{
-					$first = false;
-				}
-				else
-				{
-					echo "||";
-				}
-
-				if ($code == $lang)
-				{
-					?><strong><?php echo $langname ?></strong><?php
-				}
-				else
-				{
-					?><a href="?lang=<?php echo $code ?>&amp;version=<?php echo $version ?>"><?php echo $langname ?></a> <?php
-				}
-			}
-		?></td>
-	</tr>
-</table><!-- language and package selection -->
-</td></tr></table>
-</td></tr></table>
-
-<!-- FIXME WHY -->
-<div> <br/> </div><?php
+<div id="gettext-display-options"><?php
 
 if (!$nostats)
 {
-	?><table class="main" cellspacing="0" cellpadding="0" border="0" width="100%"><tr><td>
-	<table cellspacing="1" cellpadding="2" border="0" width="100%">
-	<tr class="header">
-		<td class="title">package</td>
-		<td class="translated">translated</td>
-		<td class="translated">%</td>
-		<td class="fuzzy"><strong>fuzzy</strong></td>
-		<td class="fuzzy"><strong>%</strong></td>
-		<td class="untranslated"><strong>untranslated</strong></td>
-		<td class="untranslated"><strong>%</strong></td>
-		<td class="title">total</td>
-		<td class="title">graph</td>
-	</tr><?php
+	$firstpack = $existing_packs[0];
+	$filestat = stat('stats/' . $firstpack . '/' . $version . 'stats');
 
-	$i=0;
-	$sumstat[1]=0;
-	$sumstat[2]=0;
-	$sumstat[3]=0;
-	$sumstat[5]=0;
+	ui_last_update_timestamp($filestat[9]);
+}
 
-	$official = 0;		//0 == official, 1 == not official
+?><div id="version">Branch:
+	<ul class="gettext-switch"
+		><li><?php ui_self_link($version == 'branch', 'Stable/1.12', "?version=branch&package=$package&lang=$lang") ?></li
+		><li><?php ui_self_link($version != 'branch', 'Development/master', "?version=master&package=$package&lang=$lang") ?></li
+	></ul>
+</div>
+
+<?php
+function ui_package_set_link($package_set, $label)
+{
+	global $version;
+
+	echo '<a href="' . htmlspecialchars("index.php?package=$package_set&order=trans&version=$version") . '">' . $label . '</a>';
+}
+
+?><div id="package-set">Show:
+	<ul class="gettext-switch"
+		><li><?php ui_package_set_link('alloff',  'All mainline textdomains')   ?></li
+		><li><?php ui_package_set_link('allcore', 'Mainline core textdomains')  ?></li
+		><li><?php ui_package_set_link('all',     'All textdomains')            ?></li
+		><li><?php ui_package_set_link('allun',   'All unofficial textdomains') ?></li
+		><li><b>By language</b></li
+	></ul>
+</div>
+
+<div id="language-teams">Language:
+	<ul class="gettext-switch"><?php
+		$sorted_langs = $langs;
+		asort($sorted_langs);
+
+		foreach ($sorted_langs as $code => $langname)
+		{
+			echo '<li>';
+			ui_self_link($code == $lang, $langname, "?lang=$code&version=$version");
+			echo '</li>';
+		}
+	?></ul>
+</div>
+
+</div><!-- gettext-display-options --><?php
+
+if (!$nostats)
+{
+	?><table class="gettext-stats">
+	<thead><tr><?php
+		?><th class="title">Textdomain</th>
+		<th class="translated">Translated</th>
+		<th class="translated percent">%</th>
+		<th class="fuzzy">Fuzzy</th>
+		<th class="fuzzy percent">%</th>
+		<th class="untranslated">Untranslated</th>
+		<th class="untranslated percent">%</th>
+		<th class="total">Total</th>
+		<th class="graph">Graph</th>
+	</tr></thead>
+	<tbody><?php
+
+	$sumstat = [ 0, 0, 0, 0, 0, 0 ];
+	$official = true;
 
 	foreach ($stats as $stat)
 	{
@@ -209,85 +170,75 @@ if (!$nostats)
 
 		$total = $stat[1] + $stat[2] + $stat[3];
 
-		$class = '-' . ($i % 2);
-
 		if ($oldofficial != $official)
 		{
-			?><tr><td></td></tr><?php
+			?><tr class="officialness-separator"><td colspan="9"></td></tr><?php
 		}
 
-		?><tr class="row<?php echo $class ?>">
-			<td><?php
-				# FIXME-GIT: this will need to be updated
-				if ($official == 0)
+		?><tr>
+			<td class="textdomain-name"><?php
+				if ($official)
 				{
 					$repo = ($version == 'master') ? 'master' : $branch;
-					echo "<strong><a href='https://raw.github.com/wesnoth/wesnoth/$repo/po/" . $stat[4]. "/$lang.po'>" . $stat[4] . '</a></strong>';
+					echo "<a class='textdomain-file' href='https://raw.github.com/wesnoth/wesnoth/$repo/po/" . $stat[4]. "/$lang.po'>" . $stat[4] . '</a>';
 				}
 				else
 				{
 					$packname = getpackage($stat[4]);
 					$repo = ($version == 'master') ? $wescamptrunkversion : $wescampbranchversion;
 					$reponame = "$packname-$repo";
-					echo "<strong><a href='https://raw.github.com/wescamp/$reponame/master/po/$lang.po'>" . $stat[4] . '</a></strong>';
+					echo "<a class='textdomain-file' href='https://raw.github.com/wescamp/$reponame/master/po/$lang.po'>" . $stat[4] . '</a>';
 				}
 			?></td><?php
 
 			if (($stat[0] == 1) || ($total == 0) || ($stat[5] == 0))
 			{
-				?><td colspan="8">Error in <?php echo $stat[4] ?> translation files</td><?php
+				?><td class="invalidstats" colspan="8">Error in <?php echo $stat[4] ?> translation files</td><?php
 			}
 			else
 			{
-				?><td align="right"><?php echo $stat[1] ?></td>
-				<td class="percentage<?php echo $class ?>" align="right"><?php printf("%0.2f", ($stat[1]*100)/$stat[5]) ?></td>
-				<td align="right"><?php echo $stat[2] ?></td>
-				<td class="percentage<?php echo $class ?>" align="right"><?php printf("%0.2f", ($stat[2]*100)/$stat[5]) ?></td>
-				<td align="right"><?php echo ($stat[5] - $stat[1] - $stat[2]) ?></td>
-				<td class="percentage<?php $class ?>" align="right"><?php printf("%0.2f", (($stat[5]-$stat[1]-$stat[2])*100)/$stat[5]) ?></td>
-				<td align="right"><?php echo $total ?></td><?php
+				?><td class="translated"><?php echo $stat[1] ?></td>
+				<td class="percent"><?php printf("%0.2f", ($stat[1]*100)/$stat[5]) ?></td>
+				<td class="fuzzy"><?php echo $stat[2] ?></td>
+				<td class="percent"><?php printf("%0.2f", ($stat[2]*100)/$stat[5]) ?></td>
+				<td class="untranslated"><?php echo ($stat[5] - $stat[1] - $stat[2]) ?></td>
+				<td class="percent"><?php printf("%0.2f", (($stat[5]-$stat[1]-$stat[2])*100)/$stat[5]) ?></td>
+				<td class="strcount"><?php echo $total ?></td><?php
 
-				$trans = sprintf("%d", ($stat[1]*200)/$stat[5]);
-				$fuzzy = sprintf("%d", ($stat[2]*200)/$stat[5]);
-				$untrans = 200 - $trans - $fuzzy;
+				$graph_width = 240; // px
 
-				?><td><img src="images/green.png" height="15" width="<?php echo $trans ?>" alt="translated"/><img src="images/blue.png" height="15" width="<?php echo $fuzzy ?>" alt="fuzzy"/><img src="images/red.png" height="15" width="<?php echo $untrans ?>" alt="untranslated"/></td><?php
+				$trans = sprintf("%d", ($stat[1] * $graph_width) / $stat[5]);
+				$fuzzy = sprintf("%d", ($stat[2] * $graph_width) / $stat[5]);
+				$untrans = $graph_width - $trans - $fuzzy;
+
+				?><td class="graph"><span class="stats-bar green-bar" style="width:<?php echo $trans ?>px"></span><span class="stats-bar blue-bar" style="width:<?php echo $fuzzy ?>px"></span><span class="stats-bar red-bar" style="width:<?php echo $untrans ?>px"></span></td><?php
 			}
 
 		?></tr><?php
-
-		++$i;
 	}
-	?><tr class="title">
-		<td>Total</td>
-		<td align="right"><?php echo $sumstat[1] ?></td>
-		<td></td>
-		<td align="right"><?php echo $sumstat[2] ?></td>
-		<td></td>
-		<td align="right"><?php echo $sumstat[3] ?></td>
-		<td></td>
-		<td align="right"><?php echo $sumstat[5] ?></td>
-		<td></td>
-	</tr>
-</table>
-</td>
-</tr>
-</table><?php
+	?></tbody>
+	<tfoot>
+		<tr>
+			<th>Total</th>
+			<td class="translated"><?php echo $sumstat[1] ?></td>
+			<td></td>
+			<td class="fuzzy"><?php echo $sumstat[2] ?></td>
+			<td></td>
+			<td class="untranslated"><?php echo $sumstat[3] ?></td>
+			<td></td>
+			<td class="strcount"><?php echo $sumstat[5] ?></td>
+			<td></td>
+		</tr>
+	</tfoot>
+	</table><?php
 }
 else
 {
+	// TODO: ask user to select a language
 	if (isset($lang))
 	{
-		?><h2>No available stats for lang <?php echo $lang ?></h2><?php
+		?><h2>No available stats for language <?php echo $lang ?></h2><?php
 	}
 }
-?><div> <br/> </div>
-<div id="footer">
-<div id="footnote">
-&copy; 2003&#8211;2016 The Battle for Wesnoth
-</div>
-</div>
-</div>
 
-</body>
-</html>
+wesmere_emit_footer();
