90581fe637bb602d483b746a02657984e425a86f
Iris Morelle <shadowm@wesnoth.org>
2018-03-21 16:01:37 -0300

    gettext.w.o: 1.14 is now the stable branch
    This also adds some WesCamp bits in a dead code path for reference
    purposes only. Don't question it.

--- gettext.wesnoth.org/includes/config.php
+++ gettext.wesnoth.org/includes/config.php
@@ -17,16 +17,16 @@ error_reporting(E_ALL);
 $msgfmt = '/usr/bin/msgfmt';
 
 // Name of the current stable branch (dir name of the Git clone).
-$branch = '1.12';
+$branch = '1.14';
 
 // Whether to actually process WesCamp content.
 $use_wescamp = false;
 
 // Name of the current stable branch for WesCamp.
-$wescamp_version_dev = '1.12';
+$wescamp_version_dev = '1.14';
 
 // Name of the current development branch for WesCamp.
-$wescamp_version_branch = '1.12';
+$wescamp_version_branch = '1.14';
 
 // Prefix containing a checkout of Wesnoth's development branch.
 $master_basedir = '/usr/src/wesnoth/master/';
