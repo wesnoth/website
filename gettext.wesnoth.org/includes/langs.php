1e6eb3477642e6d94da4a38a1abf8fb945104175
Steve Cotton <steve@octalot.co.uk>
2019-10-19 20:34:05 +0200

    removed Old English, Asturian and Friulian from g.w.o

--- gettext.wesnoth.org/includes/langs.php
+++ gettext.wesnoth.org/includes/langs.php
@@ -8,10 +8,8 @@
 
 $langs = [
 	'af'				=> 'Afrikaans',
-	'ang'				=> 'Old English',
 	'ang@latin'			=> 'Old English (Latin chars)',
 	'ar'				=> 'Arabic',
-	'ast'				=> 'Asturian',
 	'bg'				=> 'Bulgarian',
 	'ca'				=> 'Catalan',
 	'ca_ES@valencia'	=> 'Valencian (southern Catalan)',
@@ -22,7 +20,6 @@ $langs = [
 	'eu'				=> 'Basque',
 	'fi'				=> 'Finnish',
 	'fr'				=> 'French',
-	'fur_IT'			=> 'Friulian',
 	'el'				=> 'Greek',
 	'en_GB'				=> 'British English',
 	'en@shaw'			=> 'English (Shavian chars)',
